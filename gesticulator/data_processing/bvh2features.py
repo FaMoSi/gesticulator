ba820ba7288cf2d5be25a8008ad91bbf477f0fd8
Famosi <simone.faggi@yahoo.it>
2020-10-11 16:08:01 +0200

    output CSV

--- gesticulator/data_processing/bvh2features.py
+++ gesticulator/data_processing/bvh2features.py
@@ -13,7 +13,8 @@ from argparse import ArgumentParser
 import glob
 import os
 import sys
-module_path = os.path.abspath(os.path.join('..'))
+module_path = os.path.abspath(os.path.join('../visualization'))
+print("PERDIO", module_path)
 if module_path not in sys.path:
     sys.path.append(module_path)
 
@@ -43,11 +44,11 @@ def extract_joint_angles(bvh_dir, files, dest_dir, pipeline_dir, fps):
        ('jtsel', JointSelector(['Spine','Spine1','Spine2','Spine3','Neck','Neck1','Head','RightShoulder', 'RightArm', 'RightForeArm', 'RightHand', 'LeftShoulder', 'LeftArm', 'LeftForeArm', 'LeftHand'], include_root=True)),
        ('exp', MocapParameterizer('expmap')), 
        ('cnst', ConstantsRemover()),
-       ('np', Numpyfier())
+    #    ('np', Numpyfier())
     ])
 
     out_data = data_pipe.fit_transform(data_all)
-    
+
     # the datapipe will append the mirrored files to the end
     assert len(out_data) == 2*len(files)
     
@@ -56,9 +57,11 @@ def extract_joint_angles(bvh_dir, files, dest_dir, pipeline_dir, fps):
     fi=0
     for f in files:
         ff = os.path.join(dest_dir, f)
-        print(ff)
-        np.savez(ff + ".npz", clips=out_data[fi])
-        np.savez(ff + "_mirrored.npz", clips=out_data[len(files)+fi])
+        print("FILE:", ff)
+        df = pd.DataFrame(out_data[fi])
+        df.to_csv(ff + ".csv")
+        # np.savez(ff + ".npz", clips=out_data[fi])
+        # np.savez(ff + "_mirrored.npz", clips=out_data[len(files)+fi])
         fi=fi+1
 
 
@@ -67,11 +70,11 @@ if __name__ == '__main__':
 
     # Setup parameter parser
     parser = ArgumentParser(add_help=False)
-    parser.add_argument('--bvh_dir', '-orig', required=True,
-                                   help="Path where original motion files (in BVH format) are stored")
-    parser.add_argument('--dest_dir', '-dest', required=True,
-                                   help="Path where extracted motion features will be stored")
-    parser.add_argument('--pipeline_dir', '-pipe', default="../utils/",
+    parser.add_argument('--bvh_dir', '-orig', default="/Users/Famosi/Desktop/bvh_dir",
+                                help="Path where original motion files (in BVH format) are stored")
+    parser.add_argument('--dest_dir', '-dest', default="/Users/Famosi/Desktop/Thesis/Code/gesticulator/dataset/dest_dir",
+                                help="Path where extracted motion features will be stored")
+    parser.add_argument('--pipeline_dir', '-pipe', default="/Users/Famosi/Desktop/pipe_dir",
                         help="Path where the motion data processing pipeline will be stored")
 
     params = parser.parse_args()
@@ -87,4 +90,4 @@ if __name__ == '__main__':
                 basename = os.path.splitext(os.path.basename(ff))[0]
                 files.append(basename)
 
-    extract_joint_angles(params.bvh_dir, files, params.dest_dir, params.pipeline_dir , fps=60)
+    extract_joint_angles(params.bvh_dir, files, params.dest_dir, params.pipeline_dir , fps=20)
